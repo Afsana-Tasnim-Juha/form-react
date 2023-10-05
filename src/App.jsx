9e81ba7204b57da9b709d68ec265d997f54f974e
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-10-05 16:21:21 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+      <SimpleForm></SimpleForm>
+
+      <h1>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
